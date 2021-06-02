93860c6f995046ba9e71207c67c48c77812f09c7
francesco_fiorella <francescofio99@gmail.com>
2021-06-02 19:55:46 +0200

    contatti, registrazione e login fixed

--- php/login.php
+++ php/login.php
@@ -3,35 +3,49 @@ include_once '../includes/db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
     if (isset($conn)) {
-        if (isset($_POST['login'])) {
-            $username = $conn->real_escape_string($_POST['username']);
-            $password = $conn->real_escape_string($_POST['password']);
+        $username = $conn->real_escape_string($_POST['username']);
+        $password = $conn->real_escape_string($_POST['password']);
 
-            // trova l'utente
-            $user_sql = "SELECT * FROM utente WHERE username = '$username';";
-            $user_result = $conn->query($user_sql);
-            if ($user_result) {
-                if ($user_result->num_rows == 1) {
-                    $row = $user_result->fetch_array(MYSQLI_ASSOC);
-                    if (password_verify($password, $row['password'])) {
-                        session_start();
+        // trova l'utente
+        $user_sql = "SELECT * FROM utente WHERE username = '$username';";
+        $user_result = $conn->query($user_sql);
+        if ($user_result) {
+            if ($user_result->num_rows == 1) {
+                $row = $user_result->fetch_array(MYSQLI_ASSOC);
+                if (password_verify($password, $row['password'])) {
+                    session_start();
 
-                        $_SESSION['isLogged'] = true;
-                        $_SESSION['username'] = $row['username'];
-                        $_SESSION['nome'] = $row['nome'];
-                        $_SESSION['cognome'] = $row['cognome'];
-                        $_SESSION['email'] = $row['email'];
-                        header("location: ../pages/profilo_utente.php");
-                    } else {
-                        echo "La password non è corretta";
-                    }
+                    $_SESSION['isLogged'] = true;
+                    $_SESSION['username'] = $row['username'];
+                    $_SESSION['nome'] = $row['nome'];
+                    $_SESSION['cognome'] = $row['cognome'];
+                    $_SESSION['email'] = $row['email'];
+                    $output = json_encode(
+                        array(
+                            'result' => 'success'
+                        ));
                 } else {
-                    echo "Non ci sono account associati";
+                    $output = json_encode(
+                    array(
+                        'result' => 'failure',
+                        'text' => 'La password non è corretta!'
+                    ));
                 }
             } else {
-                echo "Errore in fase di login";
+                $output = json_encode(
+                    array(
+                        'result' => 'failure',
+                        'text' => 'Non ci sono account associati!'
+                    ));
             }
+        } else {
+            $output = json_encode(
+                array(
+                    'result' => 'failure',
+                    'text' => 'Errore in fase di login!'
+                ));
         }
         $conn->close();
+        die($output);
     }
 }
