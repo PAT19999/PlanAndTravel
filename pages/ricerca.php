741cbcfe08e17cf0aa36420648de0fc07231353e
Giuseppe990 <giudima99@gmail.com>
2021-06-03 15:42:57 +0200

    Funzione di ricerca che non va

--- pages/ricerca.php
+++ pages/ricerca.php
@@ -6,15 +6,55 @@ include_once '../includes/db_connection.php';
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Pagina di ricerca</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+    <style>
+        body{
+            background: darkblue;
+        }
+
+        .search-box{
+            border = 3px solid #000;
+            border-radius: 100vh 100vh;
+            width: 350px;
+            position: absolute;
+            top: 30%;
+            left: 35%;
+            background-color: #ffd000;
+        }
+
+        input{
+            width: 280px;
+            height: 50px;
+            border-radius: 100vh 0 150vh 100vh;
+            border: none;
+            outline: none;
+            padding: 0 10px;
+        }
+
+        i{
+            padding: 10px 5px;
+
+        }
+
+        button{
+            border: none;
+            background-color: #ffd000;
+            outline: none;
+            cursor: pointer;
+        }
+    </style>
+
 </head>
 <body>
 
-<form method = "post">
-    <label>Ricerca</label>
-    <input type = "text" name="search">
-    <input type = "submit" name="submit">
-</form>
+    <div class="search-box">
+        <form action="search.php" method="POST">
+            <input type="text" name="search" placeholder="Cerca una meta">
+            <button><i class="fa fa-search"></i></button>
+        </form>
+    </div>
 
 </body>
 </html>
@@ -23,37 +63,6 @@ include_once '../includes/db_connection.php';
 
 <?php
 
-$con = new PDO("mysql:host=localhost, dbname=plantraveldb", 'root', '');
-
-if (isset($_POST["submit"])){
-   $str = $_POST["search"];
-   $sth = $con->prepare("SELECT * FROM `search` WHERE name = '$str'");
-
-   $sth->setFetchMode(PDO:: FETCH_OBJ);
-   $sth->execute();
-
-   if($row = $sth->fetch()){
-        ?>
-        <br><br><br>
-        <table>
-            <tr>
-                <th>name</th>
-                <th>description</th>
-            </tr>
-            <tr>
-                <td><?php echo $row->name; ?></td>
-                <td><?php echo $row->description; ?></td>
-            </tr>
-        </table>
-
-       <?php
-   }
-
-       else{
-           echo "Risultato non trovato";
-       }
-
-}
 ?>
 
 
---
+++ pages/search.php
@@ -0,0 +1,50 @@
+<?php
+
+include_once '../includes/db_connection.php';
+
+try {
+    $db = new PDO("mysql:host=localhost","root","");
+} catch (PDOException $e){
+    echo $e -> getMessage();
+}
+
+
+
+if($_POST["search"]==""){
+    echo "<h2> Nessun risultato trovato</h2>";
+
+}else{
+    $search = trim($_POST["search"]);
+
+    $query=$db->prepare("SELECT * FROM search WHERE title like '%$search%' OR description like '%$search%'");
+    $query->execute(array());
+    $control=$query->fetchAll(PDO::FETCH_OBJ);
+    $count=$query->rowCount();
+
+    if ($count>0){ ?>
+        <table>
+            <tr style="font-size: 32px">
+                <td>Title</td>
+                <td>Description</td>
+            </tr>
+
+            <?php
+                foreach ($control as $list){?>
+                    <tr>
+                        <td><?php echo $list->title;?></td>
+                        <td><?php echo $list->description;?></td>
+                    </tr>
+                <?php }
+            ?>
+        </table>
+
+    <?php }else{
+        echo "<h2> Nessun risultato trovato</h2>";
+
+    }
+
+
+
+}
+
+?>
