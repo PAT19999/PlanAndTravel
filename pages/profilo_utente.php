1acaf2db6a5af0c927afb0c720f916ddd24fe888
Luca <lucadefazio99@gmail.com>
2021-06-05 13:13:04 +0200

    profilo agenzia

--- pages/profilo_utente.php
+++ pages/profilo_utente.php
@@ -72,6 +72,9 @@ session_start();
         <!-- Box Info profilo-->
         <div class="infoprofilo">
             <div class="dati">
+                <div class="verificato">
+                    <img src="../drawable/8224836a6169b5fb1120109040e92d96.png" alt="agenzia verificata" width="25" title="Agenzia verificata">
+                </div>
                 <h1 class="big-text">Il mio profilo</h1>
                 <hr>
                 <div class="valoriphp"><?php
